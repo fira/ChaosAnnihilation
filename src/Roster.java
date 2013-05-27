7be362e20c99456243b64e31729b91f4b32939a6
Fira <loyauflorian@gmail.com>
2013-05-27 14:46:38 +0000

    Added sample file for students roster

---
+++ src/Roster.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.List;
+
+class Roster {
+	/* List of all students to be considered */
+	private List<Student> students;
+
+	public int getSize() { return this.students.size(); }
+}
+			
