6870fd62e42aa09ce98a949b25954840e6ac4488
Fira <loyauflorian@gmail.com>
2013-05-30 09:25:43 +0000

    Changed List implementations of Roster and rewrote copy builder (using list copy duplicates references but not objects)

--- src/Roster.java
+++ src/Roster.java
@@ -1,49 +1,48 @@
 import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 class Roster {
 	/* List of all students to be considered */
-	private List<Student> students;
+	private ArrayList<Student> students;
 	/* We could check every student through assignement groups
 	 * to know those that aren't in one yet. While using a separate
 	 * attribute might be conceptually deficient by design, it is
 	 * infinitely more practical */
-	private List<Student> remainingStudents;
+	private LinkedList<Student> remainingStudents;
 	/* List of possible courses */
-	private List<Course> courses;
+	private ArrayList<Course> courses;
 
 	/* List of student groups and their associated courses */
-	private List<StudentsGroup> assignement;
+	private LinkedList<StudentsGroup> assignement;
 
 	/* Build a new, empty students roster */
 	Roster() {
-		this.students = new List<Student>;
-		this.remainingStudents = new List<Student>;
-		this.courses = new List<Course>;
-		this.assignement = new List<StudentsGroup>;
+		this.students = new ArrayList<Student>();
+		this.remainingStudents = new LinkedList<Student>();
+		this.courses = new ArrayList<Course>();
+		this.assignement = new LinkedList<StudentsGroup>();
 	}	
 	
-	/* Build a new student roster by copying an existing one */
-	Roster(Roster source) { this(source, false); }
-	Roster(Roster source, boolean clear) {
+	/* Build a new student roster by copying an existing one 
+	 * Only the students and courses are transfered, not
+	 * the assignements */
+	Roster(Roster source) {
 		/* FIXME: Assume both roster share the same students/courses
 		at all times. This should be a real copy, but all in all,
 		we don't really need it (yet?). Also speedup things. */
 		this.students = source.students;
 		this.courses = source.courses;
 		
-		if(!clear) {
-			this.remainingStudents = new List<Student>(source.remainingStudents);
-			this.assignement = new List<StudentsGroup>(source.assignement);
-		} else {
-			this.remainingStudents = new List<Student>(this.students);
-			this.assignement = new List<StudentsGroup>();
+		this.remainingStudents = new ArrayList<Student>();
+		this.assignement = new LinkedList<StudentsGroup>();
 		}
 	}
 	
 	/* Resets the roster to an unassigned state */
-	public void clearAssignements() { 
-		this.remainingStudents = new List<Student>(this.students);
+	public void clearAssignements() {
+		this.remainingStudents = new LinkedList<Student>(this.students);
 		this.assignement.clear();
 	}
 
