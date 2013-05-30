c635c26c8233da9c35b8f481d5eaa1449c8935b6
Fira <loyauflorian@gmail.com>
2013-05-30 08:30:30 +0000

    Fixed interfaces declaration. Oops!

--- src/AssignementAlgorithm.java
+++ src/AssignementAlgorithm.java
@@ -4,14 +4,10 @@ import java.util.List;
 /* Defines the interface to an algorithm that will sort
  * the current Roster. */
 public interface AssignementAlgorithm {
-	public String name;
-	
-	/* List of arguments that can be passed to the algorithm 
-	 * This is general and meant to be independant of the implementation */
-	public List<String> arguments;
-
 	/* Applies the algorithm to selected roster, and returns
-	 * a code indicative of execution status. */
-	public int apply(Roster);
+	 * a code indicative of execution status.  */
+	public int apply(Roster roster);
+	/* Get the algorithm's designation */
+	public String getName();
 }
 
--- src/HeuresticAlgorithm.java
+++ src/HeuresticAlgorithm.java
@@ -2,13 +2,12 @@ import java.util.HashMap;
 import java.util.List;
 
 public interface HeuresticAlgorithm {
-	public String name;
-	public List<String> arguments;
-
 	/* Calculate the heurestic score for a single student placed in a given
 	 * students group. Lower values are better */
 	public int getStudentScore(Student, StudentsGroup);
 	
 	/* Calculates the heurestic score for a whole student group */
 	public int getGroupScore(StudentsGroup);
+	
+	public String getName();
 }
