29f0c4ae5599f0b936dd11978264d64a335e58e0
Fira <loyauflorian@gmail.com>
2013-05-31 14:31:26 +0000

    Removed deprecated slot attribute in Course

--- src/Course.java
+++ src/Course.java
@@ -2,9 +2,6 @@
  * StudentGroups. The algorithms can then construct a Roster by assembling
  * students in groups following these courses. */
 public class Course {
-	/* Used for identification of courses filling the same planning slot 
-	 * Be careful of mistypings ! */
-	public String slotId;
 	/* User designation of the course, used for display */
 	public String name; 
 
@@ -14,15 +11,15 @@ public class Course {
 	public int minStudents;
 	public int maxStudents;
 
-	public Course(String slot, String name) {
-		this(slot, name, 1);
+	public Course(String name) {
+		this(name, 1);
 	}	
         
-	public Course(String slot, String name, int maxGroups) {
-		this(slot, name, maxGroups, 16, 24); 
+	public Course(String name, int maxGroups) {
+		this(name, maxGroups, 16, 24); 
 	}
         
-	public Course(String slot, String name, int maxGroups, int minStudents, int maxStudents) {
+	public Course(String name, int maxGroups, int minStudents, int maxStudents) {
 		this.slotId = slot;
 		this.name = name;
 		this.maxGroups = maxGroups;
